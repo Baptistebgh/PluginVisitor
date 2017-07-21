79b4c4448824ad172fd7c7f6b6a0c1bd389b45b8
Oscar <oscar.simon1995@gmail.com>
2017-07-21 16:53:32 +0200

    Nouvelle métric avec la somme des différents fichiers

--- Main.java
+++ Main.java
@@ -5,17 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 		
 		VisitorCompteur visitCount = new VisitorCompteur();
+		VisitorFiles visitFiles = new VisitorFiles();
 		VisitorSize visitSize= new VisitorSize();
 		VisitorStatic visitStatic= new VisitorStatic();
 		FilesStructure.accept(visitSize, "/home/oscar/plugin-visitor/PluginVisitor/PluginVisitor");
 		FilesStructure.accept(visitCount, "/home/oscar/plugin-visitor/PluginVisitor/PluginVisitor");
 		FilesStructure.accept(visitStatic, "/home/oscar/plugin-visitor/PluginVisitor/PluginVisitor");
+		FilesStructure.accept(visitFiles, "/home/oscar/plugin-visitor/PluginVisitor/PluginVisitor");
 		HashMap L = visitSize.getsizeL();
+		HashMap F = visitFiles.getfichiers();
 		HashMap G = visitCount.getL();
 		int T = visitStatic.getstatic();
 		System.out.println(L);
 		System.out.println(G);
 		System.out.println(" Il y a "+T+" méthodes static dans ce dossier");
+		System.out.println(F);
 	}
 
 }
---
+++ VisitorFiles.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class VisitorFiles implements Visitor{
+
+	HashMap L = new HashMap();
+	int sansextension=0;
+	int total=0;
+
+	public static <K, V extends Comparable<? super V>> HashMap sortByValue( Map<K, V> map )
+	{
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+		Collections.sort( list, new Comparator<Map.Entry<K, V>>(){
+			public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+			{
+				return (o2.getValue()).compareTo( o1.getValue() );
+			}
+		} 
+				);
+
+		HashMap result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list)
+		{
+			result.put( entry.getKey(), entry.getValue() );
+		}
+		
+		return result;
+	}
+
+	
+	public void visit(String docPath) throws IOException {
+
+		
+
+		if(testextend(docPath)){
+			if(!docPath.contains(".")){
+				sansextension=sansextension+1;
+				total=total+1;
+				L.put("sans_extension",sansextension);
+			}
+
+			else if (L.containsKey(docPath.substring(docPath.lastIndexOf(".")))){
+				int h = (int) L.get(docPath.substring(docPath.lastIndexOf(".")));
+				h=h+1;
+				L.put(docPath.substring(docPath.lastIndexOf(".")),h);
+				total=total+1;
+
+			}	
+
+			else{
+				L.put(docPath.substring(docPath.lastIndexOf(".")),1);
+				total=total+1;
+
+
+			}
+			L.put("total", total);
+
+		}
+	}
+
+	public HashMap getfichiers() {
+
+		return sortByValue(L);
+	}
+	
+	public boolean testextend(String docPath){
+		if(!docPath.startsWith(".") && !docPath.startsWith("target") && !docPath.endsWith(".otf") && !docPath.endsWith(".eot") && !docPath.endsWith(".svg") && !docPath.endsWith(".ttf") && !docPath.endsWith(".woff")
+				&& !docPath.endsWith(".woff2") && !docPath.endsWith(".gif") && !docPath.endsWith(".png") && !docPath.endsWith(".jpg") && !docPath.endsWith(".jpeg") && !docPath.endsWith(".ico") && !docPath.endsWith(".log")){
+			return true;
+		}
+		return false;
+	}
+
+}
